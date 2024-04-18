0b01c26e6afe1c919c37a5a4169ca88cc8bff1ba
JordanBandur <jordanbandur@hotmail.ca>
2024-04-18 12:42:47 -0700

    Add rest of helper functions

--- helpers.js
+++ helpers.js
@@ -8,4 +8,26 @@ const getUserByEmail = function(email, users) {
   return null;
 };
 
-module.exports = { getUserByEmail };
+const generateRandomString = function() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * characters.length); // Generates a random index for characters
+    result += characters[index]; // Adds the random generated index to select from characters
+  }
+  return result;
+};
+
+// helper which returns the URLs where the userID is equal to the id of the currently logged-in user.
+const urlsForUser = function(id, urlDatabase) {
+  const userUrls = {};
+  for (const urlId in urlDatabase) {
+    if (urlDatabase[urlId].userID === id) {
+      userUrls[urlId] = urlDatabase[urlId];
+    }
+  }
+  return userUrls;
+};
+
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
