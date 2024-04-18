82af58b812ecf7cbdd6a7e8b4e5ba611dc206fd1
JordanBandur <jordanbandur@hotmail.ca>
2024-04-18 08:58:29 -0700

    Refactor to delete only if correct user

--- express_server.js
+++ express_server.js
@@ -139,13 +139,24 @@ app.post("/urls/:id/update", (req, res) => {
 
 // Endpoint for deleting URLs
 app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id; // Get the ID from the URL parameter
-  if (urlDatabase[id]) {
-    delete urlDatabase[id];
-    res.redirect('/urls');
-  } else {
-    res.status(404).send('URL not found');
+  const user = users[req.cookies["user_id"]] || null;
+  const id = req.params.id;
+
+  // Check if user is logged in
+  if (!user) {
+    return res.status(401).send("You must be logged in to delete URLs.");
   }
+  // Check if the URL exists
+  if (!urlDatabase[id]) {
+    return res.status(404).send("The URL you are trying to delete does not exist.");
+  }
+  // Check if the logged-in user owns the URL
+  if (urlDatabase[id].userID !== user.id) {
+    return res.status(403).send("You do not have permission to delete this URL.");
+  }
+
+  delete urlDatabase[id];
+  res.redirect('/urls');
 });
 
 // Display the page for creating new URLs and pass the user object
